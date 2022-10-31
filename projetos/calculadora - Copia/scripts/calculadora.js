7359f62968dc60358a9d8c148846024756b466c6
Victor Soares <107129730+Vitin-Ps@users.noreply.github.com>
2022-10-31 00:12:53 -0300

    atualizações no projeto calculadora

---
+++ projetos/calculadora - Copia/scripts/calculadora.js
@@ -0,0 +1,258 @@
+// var do resultado
+
+let res = document.querySelector("div.resultado")
+
+//Botão Limpar
+
+function limpar() {
+    res.innerHTML = ''
+}
+
+function aptlimpar() {
+    let limpar = document.querySelector("input.limpar")
+
+    limpar.style.background = 'var(--cor3)'
+    limpar.style.color = 'var(--cor1)'
+    
+}
+
+function sltlimpar() {
+    let limpar = document.querySelector("input.limpar")
+
+    limpar.style.background = ''
+    limpar.style.color = ''
+}
+
+// Botão 0
+
+function click0() {
+    res.innerHTML += '0'
+}
+
+function apt0() {
+    let num7 = document.querySelector("input.num0")
+
+    num0.style.background = 'var(--cor3)'
+    num0.style.color = 'var(--cor1)'
+    
+}
+
+function slt0() {
+    let num7 = document.querySelector("input.num0")
+
+    num0.style.background = ''
+    num0.style.color = ''
+}
+
+// Botão 1
+
+function click1() {
+    res.innerHTML += '1'
+}
+
+function apt1() {
+    let num1 = document.querySelector("input.num1")
+
+    num1.style.background = 'var(--cor3)'
+    num1.style.color = 'var(--cor1)'
+    
+}
+
+function slt1() {
+    let num1 = document.querySelector("input.num1")
+
+    num1.style.background = ''
+    num1.style.color = ''
+}
+
+// Botão 2
+
+function click2() {
+    res.innerHTML += '2'
+}
+
+function apt2() {
+    let num2 = document.querySelector("input.num2")
+
+    num2.style.background = 'var(--cor3)'
+    num2.style.color = 'var(--cor1)'
+    
+}
+
+function slt2() {
+    let num2 = document.querySelector("input.num2")
+
+    num2.style.background = ''
+    num2.style.color = ''
+}
+
+// Botão 3
+
+function click3() {
+    res.innerHTML += '3'
+}
+
+function apt3() {
+    let num3 = document.querySelector("input.num3")
+
+    num3.style.background = 'var(--cor3)'
+    num3.style.color = 'var(--cor1)'
+    
+}
+
+function slt3() {
+    let num3 = document.querySelector("input.num3")
+
+    num3.style.background = ''
+    num3.style.color = ''
+}
+
+// Botão 4
+
+function click4() {
+    res.innerHTML += '4'
+}
+
+function apt4() {
+    let num4 = document.querySelector("input.num4")
+
+    num4.style.background = 'var(--cor3)'
+    num4.style.color = 'var(--cor1)'
+    
+}
+
+function slt4() {
+    let num4 = document.querySelector("input.num4")
+
+    num4.style.background = ''
+    num4.style.color = ''
+}
+
+// Botão 5
+
+function click5() {
+    res.innerHTML += '5'
+}
+
+function apt5() {
+    let num5 = document.querySelector("input.num5")
+
+    num5.style.background = 'var(--cor3)'
+    num5.style.color = 'var(--cor1)'
+    
+}
+
+function slt5() {
+    let num5 = document.querySelector("input.num5")
+
+    num5.style.background = ''
+    num5.style.color = ''
+}
+
+// Botão 6
+
+function click6() {
+    res.innerHTML += '6'
+}
+
+function apt6() {
+    let num6 = document.querySelector("input.num6")
+
+    num6.style.background = 'var(--cor3)'
+    num6.style.color = 'var(--cor1)'
+    
+}
+
+function slt6() {
+    let num6 = document.querySelector("input.num6")
+
+    num6.style.background = ''
+    num6.style.color = ''
+}
+
+// Botão 7
+
+function click7() {
+    res.innerHTML += '7'
+}
+
+function apt7() {
+    let num7 = document.querySelector("input.num7")
+
+    num7.style.background = 'var(--cor3)'
+    num7.style.color = 'var(--cor1)'
+    
+}
+
+function slt7() {
+    let num7 = document.querySelector("input.num7")
+
+    num7.style.background = ''
+    num7.style.color = ''
+}
+
+// Botão 8
+
+function click8() {
+    res.innerHTML += '8'
+}
+
+function apt8() {
+    let num8 = document.querySelector("input.num8")
+
+    num8.style.background = 'var(--cor3)'
+    num8.style.color = 'var(--cor1)'
+    
+}
+
+function slt8() {
+    let num8 = document.querySelector("input.num8")
+
+    num8.style.background = ''
+    num8.style.color = ''
+}
+
+// Botão 9
+
+
+function click9() {
+    res.innerHTML += '9'
+}
+
+function apt9() {
+    let num9 = document.querySelector("input.num9")
+
+    num9.style.background = 'var(--cor3)'
+    num9.style.color = 'var(--cor1)'
+    
+}
+
+function slt9() {
+    let num9 = document.querySelector("input.num9")
+
+    num9.style.background = ''
+    num9.style.color = ''
+}
+
+// Botão vírgula
+
+function clickvirg() {
+    res.innerHTML += '.'
+}
+
+function aptvirg() {
+    let virg = document.querySelector("input.numvirg")
+
+    virg.style.background = 'var(--cor3)'
+    virg.style.color = 'var(--cor1)'
+    
+}
+
+function sltvirg() {
+    let virg = document.querySelector("input.numvirg")
+
+    virg.style.background = ''
+    virg.style.color = ''
+}
+
+
