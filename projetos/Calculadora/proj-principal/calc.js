21ec4c48d4425a6d1a89489cc5586d67ce8e4353
Victor Soares <107129730+Vitin-Ps@users.noreply.github.com>
2023-03-30 20:03:53 -0300

    calculadora

--- projetos/Calculadora/proj-principal/calc.js
+++ projetos/Calculadora/proj-principal/calc.js
@@ -1,50 +1,118 @@
 let text = document.getElementById("textInput");
 let res = document.getElementById("res");
 let limpar = document.getElementById("limpar");
-
+let botoes = document.querySelectorAll('button[name="botoes"]');
+let btnRes = document.getElementById("btnRes");
+let btnOperacoes = document.querySelectorAll('button[name="btnOperacoes"]')
+let resPrimitivo = document.getElementById("resPrimitivo")
 
 let valorAnterior = [];
 let valorTotal = 0;
 let valor = 0;
 let btnAnterior = "";
 let btnIgual = false;
+let btnCE = "CE";
+let tipoOperacao = "";
+let valor01 = 0;
 
+text.value = "0";
 
-
-//função das operações matemáticas
-function calculo(valorCalc) {
-    switch (valorCalc) {
-        case 'soma': 
-            valorTotal += valor;
-            break;
-        case 'multiplicacao':
-            valorTotal = valorTotal * valor;
-            break;
-        case 'subtracao':
-            valorTotal -= valor;
-            break;
-        case 'divisao':
-            valorTotal = valorTotal / valor;
-            break;
-        case 'porcentagem':
-            valorTotal = (valorTotal / 100) * valor;
-        default:
-            window.alert("Erro")
+text.addEventListener("input", () => {
+    let txtInput = Number(text.value);
+    if(txtInput !== 0) {
+      btnRes.innerHTML = "C";
+      btnCE = "C";
+    } else {
+      btnRes.innerHTML = "CE";
+      btnCE = "CE";
     }
+  })
+
+
+botoes.forEach(botao => {
+    botao.addEventListener("click", () => {
+    let txtInput = Number(text.value);
+    if(txtInput !== 0) {
+      btnRes.innerHTML = "C";
+      btnCE = "C";
+    } else {
+      btnRes.innerHTML = "CE";
+      btnCE = "CE";
+    }
+  })
+  })
+
+function CorClara(btnVar) {
+    btnVar.style.backgroundColor = "var(--cor07)";
+    btnVar.style.borderColor = "var(--cor01)"; 
+    btnVar.style.color = "var(--cor5)";
 }
 
+function CorPadrao(btnVar) {
+    btnVar.style.backgroundColor = "var(--cor01)";
+    btnVar.style.borderColor = "var(--cor05)";
+    btnVar.style.color = "var(--cor06)";
+}
+
+function CorEscura(btnVar) {
+    btnVar.style.backgroundColor = "var(--cor05)";
+    btnVar.style.borderColor = "var(--cor01)";
+    btnVar.style.color = "var(--cor06)";
+}
+
+function funForEach(btnVar) {
+    btnVar.forEach(btn => {
+        CorPadrao(btn);
+    })
+}
+//função das operações matemáticas
+function calculo(valorCalc) {
+    
+    switch (valorCalc) {
+        case '+':
+            valorTotal += valor;
+            break;
+        case 'x':
+            valorTotal = valorTotal * valor;
+            break;
+        case '-':
+            valorTotal -= valor;
+            break;
+        case '/':
+            valorTotal = valorTotal / valor;
+            break;
+        case '%':
+            valorTotal = (valorTotal / 100) * valor;
+        default:
+            window.alert("Erro!!! Erro das Operações.")
+    }
+    valorAnterior.push(valorTotal);
+
+    res.innerHTML += `<div><p>${valor01} ${tipoOperacao}  ${valor}</p><p>=</p><p>${valorTotal}</p></div>`;
+
+    resPrimitivo.innerHTML = `<div><p>${valor01} ${tipoOperacao}  ${valor}</p><p>=</p><p>${valorTotal}</p></div>`;
+
+}
+
+
+
 function apertar(botao) {
-    botao.style.backgroundColor = "var(--cor05)";
-    botao.style.borderColor = "var(--cor01)"; // está usando o this que referencia ele mesmo
+    CorEscura(botao);// está usando o this que referencia ele mesmo
 }
 
 function soltar(botao) {
-    botao.style.backgroundColor = "var(--cor01)";
-    botao.style.borderColor = "var(--cor05)"; // está usando o this que referencia ele mesmo
+    CorPadrao(botao);
 }
 
 function mostrarNum(numero) {
-    text.value += `${numero}`;
+    CorPadrao(btnRes);
+    if (text.value == "0"){
+        text.value = `${numero}`;
+        text.focus();
+    } else {
+        text.value += `${numero}`;
+        text.focus();
+    }
 }
 
 
@@ -52,50 +120,73 @@ function operacoes(operacao) {
     if(valorTotal == 0) {
         valorTotal = Number(text.value);
         valor = 1;
-        res.innerHTML = `${valorTotal} ${valor}`;
+        valor01 = valorTotal;
+    } else if (btnIgual == true){
+        valor = Number(text.value);
+        valor01 = valorTotal;
+
     } else {
         valor = Number(text.value);
+        valor01 = valorAnterior
         calculo(btnAnterior);
-        }
+    }
+        tipoOperacao = operacao;
+        valor01 = valorTotal;
         btnAnterior = operacao;
         btnIgual = false;
-        text.value = "";
-        res.innerHTML = `${valorTotal} ${valor}`;
-        
+        text.value = "0";
     }
+
+function apertarOperacoes(botao) {
+    funForEach(btnOperacoes);
+    CorClara(botao);
+}
 
 
 function btnFuncional(func) {
     switch (func) {
-        case 'limparTotal':
-            valorAnterior = [];
-            valorTotal = 0;
-            valor = 0;
-            btnAnterior = "";
-            text.value = "";
-            res.innerHTML = "";
-            break;
-        case 'limparParcial':
-            text.value = "";
+        case 'limpar':
+            switch(btnCE) {
+                case 'C':
+                    text.value = "0";
+                    btnRes.innerHTML = "CE";
+                    btnCE = "CE";
+
+                    CorClara(btnRes);
+                    break;
+                case 'CE':
+                    valorAnterior = [];
+                    valorTotal = 0;
+                    valor = 0;
+                    btnAnterior = "";
+                    btnIgual = false;
+                    text.value = "0";
+                    res.innerHTML = "";
+                    resPrimitivo.innerHTML = "";
+
+                    funForEach(btnOperacoes);
+                    CorPadrao(btnRes);
+                    break;
+                default:
+                    window.alert("ERRO!!! Selecione Algum botão.")
+            }
             break;
         case 'igual':
-            if (text.value !== "" && btnIgual == false) {
+            if (text.value !== "0" && btnIgual == false) {
                 valor = Number(text.value);
                 btnIgual = true;
+            } else {
+                btnOperacao = btnAnterior;
             }
+            valor01 = valorTotal;
             calculo(btnAnterior);
             text.value = `${valorTotal}`;
-            res.innerHTML = `<ul>`
-            for (i = 0; i < valorTotal.length; i++) {
-                res.innerHTML += `<li>${valorAnterior[i]}</li>`;
-            }
             break;
         default:
-            window.alert("Erro!!! Clique em Algum Botão da Calculadora.")
     }
 }
 
 function funInput() {
-    text.value = "";
+    text.value = "0";
     res.innerHTML = "";
 }
