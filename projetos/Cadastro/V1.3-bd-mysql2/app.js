180c9b94e4ad7e067c4331ed380b46834d154206
Victor Soares <107129730+Vitin-Ps@users.noreply.github.com>
2023-05-12 12:04:03 -0300

    tste

--- projetos/Cadastro/V1.3-bd-mysql2/app.js
+++ projetos/Cadastro/V1.3-bd-mysql2/app.js
@@ -1,110 +1,3 @@
-// // Conexão
-
-// const express = require('express');
-// const { engine } = require('express-handlebars');
-// const bodyParser = require('body-parser');
-// const moment = require('moment');
-// const numeral = require('numeral');
-
-// //App
-// const app = express();
-// // Define a pasta onde estão os arquivos estáticos
-// app.use('/public', express.static(__dirname + '/public'));
-// app.use(bodyParser.urlencoded({ extended: true }));
-// // Tabelas do BD
-// const Vendas = require('./models/vendas');
-
-// app.engine('handlebars', engine({
-//     defaultLayout: 'main',
-//     runtimeOptions: {
-//         allowProtoMethodsByDefault: true,
-//         allowProtoPropertiesByDefault: true
-//     },
-//     helpers: {
-//         formatDate: (date) => {
-//             return moment(date).format('DD/MM/YYYY');
-//         },
-//     }
-    
-// }));
-// app.set('view engine', 'handlebars');
-// app.set('views', './views');
-
-// numeral.defaultFormat('0,0.00');
-
-// // ------------------------- Rotas --------------------
-
-// app.get('/cadastro-vendas', (req, res) => {
-//     Vendas.findAll().then((vendas) => {
-//         res.render('cadastro-vendas', {vendas: vendas});
-//     })
-// })
-
-// app.post('/add-venda', (req, res) => {
-//     const valorVenda = parseFloat(req.body.valorVenda);
-//     // const desconto = valorVenda * 0.1;
-//     const desconto = numeral(valorVenda + 0.1).format();
-
-//     Vendas.create({
-//         nomeFunc: req.body.nomeFunc,
-//         valorVenda: valorVenda,
-//         desconto: desconto
-//     }).then(function(){
-//         res.redirect("/cadastro-vendas");
-//     }).catch(function(erro){
-//         res.send("Erro ao enviar as informações " + erro);
-//     });
-// });
-
-// app.get('/del-venda/:idFunc', (req, res) => {
-//     Vendas.destroy({
-//         where: {'idFunc': req.params.idFunc}
-//     }).then(() => {
-//         res.redirect("/cadastro-vendas");
-//     }).catch((erro) => {
-//         ("Não Apagado " + erro);
-//     });
-// });
-
-// app.get('/editar-venda/:idFunc', (req, res) => {
-//     Vendas.findByPk(req.params.idFunc).then((vendas) => {
-//         res.render('editar-venda', {idFunc: vendas.idFunc, nomeFunc: vendas.nomeFunc, valorVenda: vendas.valorVenda, desconto: vendas.desconto});
-//     }).catch((erro) => {
-//         res.send("Dado não encontrado " + erro);
-//     });
-// });
-
-// app.post('/alt-venda/:idFunc', (req, res) => {
-//     const valorVenda = parseFloat(req.body.valorVenda);
-//     // const desconto = valorVenda * 0.1;
-//     const desconto = numeral(valorVenda + 0.1).format();
-
-   
-//     Vendas.update({
-//         nomeFunc: req.body.nomeFunc,
-//         valorVenda: valorVenda,
-//         desconto: desconto
-//     },
-//     {
-//         where: {'idFunc': req.body.idFunc}
-//     }).then(() => {
-//         res.redirect("/cadastro-vendas");
-//     }).catch((erro) => {
-//         res.send("Não Alterado " + erro);
-//     });
-// })
-
-// app.get('/limpar-tabela-vendas', (req, res) => {
-//     Vendas.destroy({ where: {} }).then(() => {
-//         res.redirect('/cadastro-vendas');
-//       }).catch((erro) => {
-//         res.send('Erro ao excluir registros:', erro);
-//       });
-//   });
-  
-
-// app.listen(8081);
-
 
 //express
 const express = require('express');
@@ -130,7 +23,7 @@ app.set('view engine', 'handlebars'); // meio que sincronisa o express para que 
 app.set('views', './views'); // ativa o handlebars para buscar na pasta /views os arquivos que vão ser usados como html
 
 const Vendas = require('./models/vendas');
-//Vendas.criarVendas(); //usado para criar a tabela(use somente na primeira aplicação)
+Vendas.criarVendas(); //usado para criar a tabela(use somente na primeira aplicação)
 
 //rotas
 app.get('/cadastro-vendas', async (req, res) => { // aqui abre uma rota no express que cria uma aba no url 
--- projetos/Cadastro/V1.3-bd-mysql2/ex.sql
+++
@@ -1,6 +0,0 @@
-create table vendas(
-    idFunc int primary key auto-increment,
-    nomeFunc varchar(80) not null,
-    valorVenda decimal(10,2) not null,
-    desconto decimal(10,2)
-);
