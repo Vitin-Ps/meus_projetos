f04bb967ec4f41de0a32b9b6539516a7b464373d
Victor Soares <107129730+Vitin-Ps@users.noreply.github.com>
2023-06-14 16:17:26 -0300

    cad_vendas sem css

--- projetos/Cadastro/JAVA/cad_usuario/src/main/java/com/crud/cad_usuario/controller/CrudController.java
+++ projetos/Cadastro/JAVA/cad_usuario/src/main/java/com/crud/cad_usuario/controller/CrudController.java
@@ -1,20 +1,21 @@
 package com.crud.cad_usuario.controller;
 
+import com.crud.cad_usuario.model.DAO.CrudDAO;
 import com.crud.cad_usuario.model.funcionario.DadosAlteracaoFuncionario;
 import com.crud.cad_usuario.model.funcionario.DadosCadastroFuncionario;
 import com.crud.cad_usuario.model.funcionario.Funcionario;
 import com.crud.cad_usuario.model.funcionario.FuncionarioRepository;
-import com.crud.cad_usuario.model.venda.DadosAlteracaoVenda;
-import com.crud.cad_usuario.model.venda.DadosCadastroVenda;
-import com.crud.cad_usuario.model.venda.Venda;
-import com.crud.cad_usuario.model.venda.VendaRepository;
+import com.crud.cad_usuario.model.venda.*;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.Query;
+import ognl.ObjectElementsAccessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
 
 @Controller // é uma anotação usada para marcar uma classe como um controlador no contexto do Spring MVC. Essa anotação indica que a classe é responsável por lidar com as requisições HTTP e retornar as respostas correspondentes.
 @RequestMapping // usada para mapear uma requisição HTTP para um método específico dentro de um controlador.
@@ -25,7 +26,7 @@ public class CrudController {
     @Autowired
     private VendaRepository repositoryVenda;
     @Autowired
-    private EntityManager entityManager;
+    private EntityManager entityManager; // classe do Jpa usada para mapear objetos para tabelas do banco de dados
 
 
 
@@ -34,30 +35,6 @@ public class CrudController {
     public String paginaPricipal(){
         return "index";
     }
-
-
-
-    @PostMapping("/enviaNomeFunc")
-    @Transactional
-    public void mudaVariavel(@RequestBody String selectedValue){
-        System.out.println("Valor selecionado: " + selectedValue);
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -104,11 +81,12 @@ public class CrudController {
 
     @GetMapping("/vendas") // criando um link --> localhost:8080/vendas
     public String paginaVendas(Model model,  Long idVenda){ // pega o id pelo link
+
         if(idVenda != null) { // se o id for diferente de null, no cado da alteração
             var venda = repositoryVenda.getReferenceById(idVenda); // ele busca as informações por id
             model.addAttribute("venda", venda); // joga essas inormações numa variavel web
         }
-        this.listaModelFunc(model,"funcionarios");
+        this.listaModelFunc(model,"funcionarios"); // para o select dos nomes dos funcionarios
         this.listaModelVenda(model, "vendas");  // chama o método para enviar as informações para variavel web
         return "subPage/vendas";
     }
@@ -116,6 +94,8 @@ public class CrudController {
     @PostMapping("/cadVenda") // mapeia uma requisição http do tipo Post
     @Transactional // garante que a operação de salvar o funcionário no banco de dados seja realizada dentro de uma transação.
     public String cadVenda(DadosCadastroVenda dados) { // vai receber as infromações via post e mandalas para o record
+        int desconto = CrudDAO.consultarDesconto(entityManager, dados.funcionario()); // aqui buscamos o desconto_func
+        CrudDAO.mudarPorcentagem(entityManager, desconto); // aqui mudamos a porcentagem
         var vendas = new Venda(dados); // vai usar o construtor que muda os dados, usando os dados do record
         repositoryVenda.save(vendas); // vai salvar as informações do objeto em um método do jpa que deve ser um insert into e executalo no banco
         return "redirect:/vendas"; // redireciona a página
@@ -131,6 +111,8 @@ public class CrudController {
     @Transactional
     public String altVenda (DadosAlteracaoVenda dados) {// vai pegar as informações do record alterarDados
         var venda = repositoryVenda.getReferenceById(dados.idVenda()); // vai pegar as informações da tabela baseado no id
+        int desconto = CrudDAO.consultarDesconto(entityManager, dados.funcionario());
+        CrudDAO.mudarPorcentagem(entityManager, desconto);
         venda.atualizaDados(dados); // usa um método jpa para atualizar os dados
         return "redirect:/vendas";
     }
@@ -138,6 +120,26 @@ public class CrudController {
     private void listaModelVenda(Model model, String atributoWeb) { // a classe Model é usada para transportar as informações do banco para um array que poderá ser mostrado na web
         model.addAttribute(atributoWeb, repositoryVenda.findAll()); // vai levar para a variável atributoWeb no html as informações do objeto que no caso é um método da jpa, que deve usar um SELECT *
     }
+
+    // ------------------------ SOMAVALORES --------------------------
+
+    @GetMapping("/somaValores")
+    @Transactional
+    public String paginaValores(Model model, String func) { // recebe a varivel do post pelo link --> obs; tem que se o mesmo nome que está no link
+        this.listaModelFunc(model, "funcionarios"); // retorna os nome dos funcionarios no formulario
+        Set<SomaValores> listaValores = CrudDAO.obterValoresFunc(entityManager, func); // obtem valores do funcionario
+        Set<SomaValores> soma = CrudDAO.obterSomaVendasDescontos(entityManager, func);
+        model.addAttribute("vendas", listaValores); // mostra os valores na lista, com a varivel vendas para web
+        model.addAttribute("somaValores", soma); // mostra a oma atravez da variavel somaValores na web
+        return "subPage/somaValores";
+    }
+
+    @PostMapping("/buscaFunc")
+    @Transactional
+    public String buscaFuncionario(String funcionario) { // ao clicar em enviar ele recebe a fincomação do input e guarda na string funcionario
+        return "redirect:/somaValores?func=" + funcionario; // envia a variavel funcionario pelo get
+    }
+    // ao clicar em enviar ele recebe a fincomação do input e guarda na string funcionario
 
 
 }
--- projetos/Cadastro/JAVA/cad_usuario/src/main/resources/cad_usuario_bd.sql
+++ projetos/Cadastro/JAVA/cad_usuario/src/main/resources/cad_usuario_bd.sql
@@ -1,13 +1,13 @@
-DROP DATABASE IF EXISTS cad_vendas;
+DROP DATABASE IF EXISTS cad_vendas; -- se o database existir, exclua
 
-CREATE DATABASE IF NOT EXISTS cad_vendas;
+CREATE DATABASE IF NOT EXISTS cad_vendas; -- se o database não existir, crie
 
-USE cad_vendas;
+USE cad_vendas; -- usar database
 
 CREATE TABLE funcionarios (
     id_func BIGINT PRIMARY KEY AUTO_INCREMENT,
     nome_func VARCHAR(80) NOT NULL,
-    matricula VARCHAR(80) NOT NULL UNIQUE,
+    matricula VARCHAR(80) NOT NULL UNIQUE, -- unique significa que a matricula é unica, não pode ser repetida
     numero VARCHAR(20) NOT NULL,
     email VARCHAR(80) NOT NULL,
     desconto_func INT NOT NULL
@@ -21,22 +21,48 @@ CREATE TABLE vendas (
     desconto_venda DOUBLE(10,2)
 );
 
-
--- SET @porcentagemDesconto = ?;
--- SET @desconto = @porcentagemDesconto * 0.01;
-
--- SELECT @desconto;
--- SELECT @porcentagemDesconto;
-
+-- Trigger para inserção
 DELIMITER //
 
 CREATE TRIGGER inserir_desconto
-BEFORE INSERT ON vendas
+BEFORE INSERT ON vendas -- depois de inserir na tabela vendas
 FOR EACH ROW
 BEGIN
-    SET NEW.desconto_venda = NEW.valor_venda * (@porcentagemDesconto * 0.01);
+    IF NEW.desconto_venda IS NULL THEN -- se desconto_venda for null
+        SET NEW.desconto_venda = NEW.valor_venda * (@porcentagemDesconto * 0.01); -- mudar desconto_venda baseado na variável alterada pelo backend
+    END IF;
 END //
 
 DELIMITER ;
 
 
+-- Trigger para atualização
+DELIMITER //
+
+CREATE TRIGGER atualizar_desconto -- mesma coisa só que para atualização
+BEFORE UPDATE ON vendas
+FOR EACH ROW
+BEGIN
+    IF NEW.desconto_venda IS NULL THEN
+        SET NEW.desconto_venda = NEW.valor_venda * (@porcentagemDesconto * 0.01);
+    END IF;
+END //
+
+DELIMITER ;
+
+-- alteração de nome e desconto baseado na tabela funcionarios
+
+
+DELIMITER //
+
+CREATE TRIGGER atualizar_funcionario_vendas
+BEFORE UPDATE ON funcionarios -- depois de atualizar a tabela funcionarios 
+FOR EACH ROW
+BEGIN
+    UPDATE vendas
+    SET funcionario = NEW.nome_func, -- mudar funcionario, baseado na alteração de funcionarios
+		desconto_venda = vendas.valor_venda * (NEW.desconto_func * 0.01) -- mudar desconto_venda baseado na alteração de desconto_func
+    WHERE funcionario = OLD.nome_func; -- com o parametro do mome que foi alterado, por isso OLD
+END //
+
+DELIMITER ;
