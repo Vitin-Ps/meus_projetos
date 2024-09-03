5d0c7ac7cd0f949ceb0ebcd878ef1602f5e499c5
Vitin-Ps <vicssoares2802@gmail.com>
2024-09-03 12:02:00 -0300

    aprendendo rolagem de imagens

---
+++ testes/carousel_imagens/carousel/script.js
@@ -0,0 +1,16 @@
+const imgs = document.getElementById('img');
+const img = document.querySelectorAll("#img img");
+
+let idX = 0;
+
+function carousel () {
+    idX ++;
+
+    if(idX > img.length - 1) {
+        idX = 0;
+    } 
+
+    imgs.style.transform = `translateX(${-idX * 500}px)`;
+}
+
+setInterval(carousel, 1800);
---
+++ testes/carousel_imagens/slider/V2/script.js
@@ -0,0 +1,136 @@
+let imagens = [
+    {
+        id: 1,
+        alt: 'Imagem 1',
+        url: 'https://get.pxhere.com/photo/disney-mickey-mouse-castillo-walt-disney-world-sky-amusement-park-landmark-world-night-castle-park-architecture-recreation-nonbuilding-structure-water-castle-space-vehicle-tourist-attraction-evening-1592179.jpg'
+    },
+    {
+        id: 2,
+        alt: 'Imagem 2',
+        url: 'https://imagens.usp.br/wp-content/uploads/Ciclovia-da-cidade-Universitaria_Foto-Marcos-Santos_U0Y3367-2048x1365.jpg'
+    },
+    {
+        id: 3,
+        alt: 'Imagem 3',
+        url: 'https://c.pxhere.com/photos/6c/e0/blue_clouds_day_fluffy_sky_summer_nature-1248502.jpg!d'
+    },
+    {
+        id: 4,
+        alt: 'Imagem 4',
+        url: 'https://get.pxhere.com/photo/disney-mickey-mouse-castillo-walt-disney-world-sky-amusement-park-landmark-world-night-castle-park-architecture-recreation-nonbuilding-structure-water-castle-space-vehicle-tourist-attraction-evening-1592179.jpg'
+    },
+    {
+        id: 5,
+        alt: 'Imagem 5',
+        url: 'https://imagens.usp.br/wp-content/uploads/Ciclovia-da-cidade-Universitaria_Foto-Marcos-Santos_U0Y3367-2048x1365.jpg'
+    },
+    {
+        id: 6,
+        alt: 'Imagem 6',
+        url: 'https://c.pxhere.com/photos/6c/e0/blue_clouds_day_fluffy_sky_summer_nature-1248502.jpg!d'
+    },
+]
+
+var slider = document.querySelector(".slider");
+var slides = document.querySelector(".slides");
+// var radiosContainer = document.querySelector(".radio-btns-container");
+var navigationAuto = document.querySelector(".navigation-auto");
+var manualNavigation = document.querySelector(".manual-navigation");
+
+var setaDireita = document.querySelector(".seta_direita");
+var setaEsquerda = document.querySelector(".seta_esquerda");
+
+const carregarImagens = () => {
+
+    imagens.forEach((imagem) => {
+
+        //imagens
+        const img = document.createElement('img');
+        img.src = imagem.url;
+        img.alt = imagem.alt;
+        slides.appendChild(img);
+
+
+        //radios
+        // const radio = document.createElement('input');
+        // radio.type = 'radio';
+        // radio.name = 'radio-btn';
+        // radio.id = `radio_${imagem.id}`;
+
+        // radiosContainer.appendChild(radio);
+
+        // navigatio auto
+        const btnAuto = document.createElement('div');
+        btnAuto.classList.add(`auto-btn_${imagem.id}`);       
+
+        navigationAuto.appendChild(btnAuto)
+
+        // navigation manual
+        const labelManual = document.createElement('div');
+        labelManual.id = `radio_${imagem.id}`;
+        labelManual.classList.add('manual-btn');
+        labelManual.addEventListener('click', () => {
+            const id = labelManual.id.split('_')[1];
+            selecionarImagem(id);
+        })
+
+        manualNavigation.appendChild(labelManual);
+    });
+
+    const btnAuto = navigationAuto.querySelectorAll('*')[0];
+    btnAuto.style.backgroundColor = '#FFF'
+}
+
+const selecionarImagem = (id) => {    
+    const btnsAuto = navigationAuto.querySelectorAll('*');
+    const btnAuto = Array.from(btnsAuto).find((btnAuto) => btnAuto.className ===`auto-btn_${id}`);
+
+    btnsAuto.forEach((btnAuto) => {
+        btnAuto.style.backgroundColor = 'transparent';
+    });
+
+    btnAuto.style.backgroundColor = '#FFF'
+
+    slides.style.transform = `translateX(${-(id -1) * 500}px)`;
+}
+
+setaDireita.addEventListener('click', () => {
+    if(count > imagens.length - 1) {
+        count = 1;
+    } else {
+        count++;
+    }
+
+    selecionarImagem(imagens[count - 1].id);
+})
+
+setaEsquerda.addEventListener('click', () => {
+    if(count === 1) {
+        count = imagens.length;
+    } else {
+        count--;
+    }
+
+    selecionarImagem(imagens[count - 1].id);
+})
+
+
+carregarImagens();
+
+let count = 1;
+
+// document.getElementById("radio1").checked = true;
+
+// setInterval(() => {
+//     nextImage();
+// }, 2000);
+
+
+function nextImage() {
+    count++;
+    if(count > 6) {
+        count = 1;
+    }
+
+    document.getElementById(`radio${count}`).checked = true;
+}
